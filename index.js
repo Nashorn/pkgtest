8cf645e1a0de758314ef57621917ff26346782eb
Jason E. Smith <jaysmith024@gmail.com>
2021-01-22 12:12:04 -0500

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+var http_server = require("http-server");
+const cwd = process.cwd();
+var exec = require('child_process').exec;
+var s = process.execPath;
+    s = s.substr(0,s.lastIndexOf("/"));
+
+
+
+process.chdir(s);
+// exec(`http-server ${s} -o index.html`)
+exec(`npm start`);
+
+console.log("process.execPath",s);
+console.log("process.versions",process.versions)
+if(http_server){
+    console.log("http_server", "successs");
+}else{
+    console.log("http_server", "Server module not found")
+}

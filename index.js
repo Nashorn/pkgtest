262c0d2fe9529a5ea57deaff7503c2bcec6429f5
Jason E. Smith <jaysmith024@gmail.com>
2021-01-25 09:44:18 -0500

    build 6

--- index.js
+++ index.js
@@ -7,19 +7,16 @@ var s = process.execPath;
     s = s.substr(0,s.lastIndexOf("/"));
 
 
-    console.log("app path",s);
-    console.log("process.execPath",path.dirname(process.execPath));
-    console.log("process.cwd",process.cwd());
-    console.log("process.pkg",process.pkg);
-    console.log("process.versions",process.versions)
+    // console.log("app path",s);
+    // console.log("process.execPath",path.dirname(process.execPath));
+    // console.log("process.cwd",process.cwd());
+    // console.log("process.pkg",process.pkg);
+    // console.log("process.versions",process.versions)
 
     exec("kill-port --port 8080,8081,8082,8083,8084,8085,8086,8087,8088,8089,8090,8091,8092,8093,8094,8095");
     setTimeout(e => {
         kill([8080,8081,8082,8083,8084,8085,8086,8087,8088,8089,8090,8091,8092,8093,8094,8095], 'tcp')
             .then(e => {
-                    process.chdir(path.dirname(process.execPath));
-                    exec(`npm start`);
-                    
                     console.log("app path",s);
                     console.log("process.execPath",path.dirname(process.execPath));
                     console.log("process.cwd",process.cwd());
@@ -29,8 +26,15 @@ var s = process.execPath;
                         console.log("http_server", "successs");
                     }else{
                         console.log("http_server", "Server module not found")
-                    }        
+                    } 
+
+                    // process.chdir(s);
+                    // process.chdir(path.dirname(process.execPath));
+                    console.log("Starting npm");
+                    exec(`npm start`);
+                    
+                           
             })
             .catch(console.log)
     },2000);
-    process.chdir(s);
+    // process.chdir(s);

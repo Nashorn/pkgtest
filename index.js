baa968fe92cb181fcc5bcbb45a8691ca8acbd29f
Jason E. Smith <jaysmith024@gmail.com>
2021-01-25 09:12:25 -0500

    win build test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http_server = require("http-server");
+const kill = require('kill-port');
+var path = require("path");
 const cwd = process.cwd();
 var exec = require('child_process').exec;
 var s = process.execPath;
@@ -6,14 +8,58 @@ var s = process.execPath;
 
 
 
-process.chdir(s);
-// exec(`http-server ${process.execPath} -o index.html`)
-exec(`npm start`);
+    exec("kill-port --port 8080,8081,8082,8083,8084,8085,8086,8087,8088,8089,8090,8091,8092,8093,8094,8095");
+    setTimeout(e => {
+        kill([8080,8081,8082,8083,8084,8085,8086,8087,8088,8089,8090,8091,8092,8093,8094,8095], 'tcp')
+            .then(e => {
+                    process.chdir(s);
+                    // exec(`http-server ${process.execPath} -o index.html`)
+                    exec(`npm start`);
+                    
+                    console.log("app path",s);
+                    console.log("process.execPath",path.dirname(process.execPath));
+                    console.log("process.cwd",process.cwd());
+                    console.log("process.pkg",process.pkg);
+                    console.log("process.versions",process.versions)
+                    if(http_server){
+                        console.log("http_server", "successs");
+                    }else{
+                        console.log("http_server", "Server module not found")
+                    }        
+            })
+            .catch(console.log)
+        // // exec(`http-server ${process.execPath} -o index.html`)
+        // exec(`npm start`);
+            
+        // console.log("app path",s);
+        // console.log("process.execPath",path.dirname(process.execPath));
+        // console.log("process.cwd",process.cwd());
+        // console.log("process.pkg",process.pkg);
+        // console.log("process.versions",process.versions)
+        // if(http_server){
+        //     console.log("http_server", "successs");
+        // }else{
+        //     console.log("http_server", "Server module not found")
+        // } 
+    },2000);
+    process.chdir(s);
+            
+    // kill(8085, 'tcp')
+    //   .then(e => {
+    //         process.chdir(s);
+    //         // exec(`http-server ${process.execPath} -o index.html`)
+    //         exec(`npm start`);
+            
+    //         console.log("app path",s);
+    //         console.log("process.execPath",path.dirname(process.execPath));
+    //         console.log("process.cwd",process.cwd());
+    //         console.log("process.pkg",process.pkg);
+    //         console.log("process.versions",process.versions)
+    //         if(http_server){
+    //             console.log("http_server", "successs");
+    //         }else{
+    //             console.log("http_server", "Server module not found")
+    //         }        
+    //   })
+    //   .catch(console.log)
 
-console.log("process.execPath",process.execPath);
-console.log("process.versions",process.versions)
-if(http_server){
-    console.log("http_server", "successs");
-}else{
-    console.log("http_server", "Server module not found")
-}
